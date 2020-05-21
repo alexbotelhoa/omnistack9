eb2ef429e0745563ca1dc223426b00fe55e633ee
Alex Botelho <alexbotelho1@hotmail.com>
2020-05-21 08:45:32 -0300

    Instalação de Resource

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,27 @@
 const express = require('express')
+    , resource = require('express-resource')
+    , routes = express()
 const multer = require('multer')
 const uploadConfig = require('./config/upload')
+const upload = multer(uploadConfig)
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
 const DashboardController = require('./controllers/DashboardController')
 const BookingController = require('./controllers/BookingController')
 
-const upload = multer(uploadConfig)
-const routes = express.Router()
-
 routes.get('/', (req, res) => res.send('Ok!'))
 
-routes.get('/sessions', SessionController.index)
-routes.post('/sessions', SessionController.store)
+/**
+ * GET     /                 ->  index
+ * GET     /new              ->  new
+ * POST    /                 ->  create
+ * GET     /:id              ->  show
+ * GET     /:id/edit         ->  edit
+ * PUT     /:id              ->  update
+ * DELETE  /:id              ->  destroy
+ * */
+routes.resource('sessions', SessionController)
 
 routes.get('/spots', SpotController.index)
 routes.post('/spots', upload.single('thumbnail'), SpotController.store)
